e1308beaba71613635a27965d1eb0630fba35f8f
Antoine Hakim <thenoobgrammer@gmail.com>
2023-07-07 12:26:31 -0400

    * updates

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : ''
-export const AUTH_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : ''
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/v1' : '/'
--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -27,7 +27,7 @@ const accessToken = window.localStorage.getItem('auth.accessToken')
 
 const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ?
-        '/' : 'http://localhost:8000',
+        '/' : 'http://localhost:8000/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'X-Request-Id': uuidv4(),
