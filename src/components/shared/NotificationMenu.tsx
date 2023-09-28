1ee0e0bb002a10b5681c3befa061d06202344290
Antoine Hakim <thenoobgrammer@gmail.com>
2023-09-27 23:20:36 -0400

    * fix notification

--- src/components/shared/NotificationMenu.tsx
+++ src/components/shared/NotificationMenu.tsx
@@ -1,37 +1,42 @@
-import { Dropdown, MenuItem } from '@components'
+import { Dropdown, MenuItem, Spinner } from '@components'
+import { useFetchNotifications, useReadNotification } from '@hooks'
 
-import { AppState } from '@state'
 import { FC } from 'react'
 import { MdOutlineNotifications } from 'react-icons/md'
 import { RxDotFilled } from 'react-icons/rx'
-import { useReadNotification } from '@hooks'
-import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 
 const NotificationMenu: FC<any> = () => {
 	const { readNotification } = useReadNotification()
 
 	const { t } = useTranslation()
-	const notifications = useSelector((state: AppState) => state.notifications)
 
+	const { notifications, isLoading } = useFetchNotifications()
+	console.log(notifications)
 	return (
-		<div>
-			<Dropdown
-				variant="secondary"
-				text={t('Notifications')}
-				start={<MdOutlineNotifications size={20} />}
-				badge={notifications?.results?.some((n) => !n.isRead) && <RxDotFilled className="text-blue-400" size={20} />}
-			>
-				{notifications?.results?.map((notification, idx) => (
+		<Dropdown
+			variant="secondary"
+			text={t('Notifications')}
+			start={<MdOutlineNotifications size={20} />}
+			badge={
+				notifications?.data?.results?.some((n) => !n.isRead) && <RxDotFilled className="text-blue-400" size={20} />
+			}
+		>
+			{isLoading ? (
+				<MenuItem>
+					<Spinner />
+				</MenuItem>
+			) : (
+				notifications?.data?.results?.map((notification, idx) => (
 					<MenuItem key={idx} onClick={() => readNotification(notification.id)}>
 						<div className="relative flex justify-between">
 							{!notification.isRead && <RxDotFilled className="text-blue-400" size={20} />}
 							{notification.message}
 						</div>
 					</MenuItem>
-				))}
-			</Dropdown>
-		</div>
+				))
+			)}
+		</Dropdown>
 	)
 }
 export default NotificationMenu
