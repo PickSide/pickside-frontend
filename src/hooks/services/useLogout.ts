1460595dd662a546cbc327ce9485a86dab090d23
thenoobgrammer <thenoobgrammer@gmail.com>
2023-10-26 17:59:47 -0400

    * updates

--- src/hooks/services/useLogout.ts
+++ src/hooks/services/useLogout.ts
@@ -2,11 +2,17 @@ import { AxiosContext } from '@context'
 import { setUserEmpty } from '@state'
 import { useContext } from 'react'
 import { useDispatch } from 'react-redux'
+import { useLocalStorage } from 'react-use'
 import { useMutation } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
 
 const useLogout = () => {
 	const { axiosInstance } = useContext(AxiosContext)
 	const dispatch = useDispatch()
+	const [, , removeCachedUser] = useLocalStorage('user')
+	const [, , removeAccessToken] = useLocalStorage('accessToken')
+	const [, , removeRefreshToken] = useLocalStorage('refreshToken')
+	const { t } = useTranslation()
 
 	const callback = async () => await axiosInstance.post(`/logout`)
 
@@ -17,7 +23,19 @@ const useLogout = () => {
 		isError,
 	} = useMutation(callback, {
 		mutationKey: ['logoutUser'],
-		onSuccess: () => dispatch(setUserEmpty()),
+		onSuccess: () => {
+			removeCachedUser()
+			removeAccessToken()
+			removeRefreshToken()
+			dispatch(setUserEmpty())
+			dispatch({
+				type: 'toast/toastMessage',
+				payload: {
+					message: t('Successfully logged out'),
+					type: 'success',
+				},
+			})
+		},
 		onError: (e) => console.log(e),
 	})
 
