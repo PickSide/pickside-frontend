6c61427cf8f1b1e973aaa59399e40db9bead83c1
thenoobgrammer <thenoobgrammer@gmail.com>
2024-01-06 20:57:10 -0500

    * fix switch minor bug

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,9 +1,9 @@
-import { ComponentPropsWithRef, forwardRef } from 'react'
+import { ComponentPropsWithRef, forwardRef, useCallback, useState } from 'react'
 import { VariantProps, cva } from 'class-variance-authority'
 
 import { cn } from '@utils'
 
-export const SwitchVariants = cva(
+export const switchVariants = cva(
 	[
 		'cursor-pointer',
 		'rounded-full',
@@ -47,19 +47,27 @@ export const SwitchVariants = cva(
 	},
 )
 
-export interface SwitchProps extends ComponentPropsWithRef<'label'>, VariantProps<typeof SwitchVariants> {
-	checked?: boolean
-	defaultChecked?: boolean
+export interface SwitchProps extends ComponentPropsWithRef<'label'>, VariantProps<typeof switchVariants> {
 	label?: string
 	onChange?: (e) => void
 }
 
-const Switch = forwardRef<any, SwitchProps>(
-	({ className, size, variant, checked = false, defaultChecked = false, label, onChange, ...rest }, ref) => {
+const Switch = forwardRef<HTMLInputElement, SwitchProps>(
+	({ className, size, variant, defaultChecked = false, label, onChange, ...rest }, ref) => {
+		const [checked, setChecked] = useState<boolean>(defaultChecked)
+
+		const handleChange = useCallback(
+			(e) => {
+				setChecked((prev) => !prev)
+				onChange && onChange(e)
+			},
+			[onChange],
+		)
+
 		return (
-			<label ref={ref} className="inline-flex items-center relative cursor-pointer"  {...rest}>
-				<input className="sr-only peer" type="checkbox" checked={checked} defaultChecked={defaultChecked} onChange={onChange} />
-				<div className={cn(SwitchVariants({ className, size, variant }), className)} />
+			<label className="inline-flex items-center relative cursor-pointer">
+				<input ref={ref} className="sr-only peer" type="checkbox" checked={checked} onChange={handleChange} />
+				<div className={cn(switchVariants({ className, size, variant }), className)} />
 				{label && <span className="ml-3 text-md font-medium text-charcoal-black">{label}</span>}
 			</label>
 		)
