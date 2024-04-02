b30492af2afd716a676ff44cd99ed8d1ad2d30bf
thenoobgrammer <thenoobgrammer@gmail.com>
2024-04-01 22:13:56 -0400

    * fix notification service link * bump minor version

--- src/hooks/services/useFetchNotifications.ts
+++ src/hooks/services/useFetchNotifications.ts
@@ -11,7 +11,7 @@ const useFetchNotifications = () => {
 
 	const me = useSelector((state: AppState) => state.user)
 
-	const callback = async () => await axiosNSInstance.get<any>(`/notifications/users/${me?.id}`)
+	const callback = async () => await axiosNSInstance.get<any>(`/users/${me?.id}`)
 
 	const {
 		data: notifications,
