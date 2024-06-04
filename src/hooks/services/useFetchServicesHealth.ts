fdcfb6d70bca6c297deafd7c56e94e6f01510226
thenoobgrammer <thenoobgrammer@gmail.com>
2024-06-04 04:22:30 -0400

    update build fix

--- src/hooks/services/useFetchServicesHealth.ts
+++ src/hooks/services/useFetchServicesHealth.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { AxiosContext } from '@context'
 import { useContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
@@ -43,49 +42,3 @@ const useFetchServicesHealth = () => {
 }
 
 export default useFetchServicesHealth
-=======
-import { AxiosContext } from '@context'
-import { useContext } from 'react'
-import { useQuery } from '@tanstack/react-query'
-
-const useFetchServicesHealth = () => {
-	const { axiosASInstance, axiosFSInstance, axiosMSInstance, axiosMSGSInstance, axiosNSInstance } = useContext(AxiosContext)
-
-	const fetchServicesHealth = async () => {
-		const promiseHandler = (promise, serviceName: string) => {
-			return promise
-				.then((response) => ({ name: serviceName, success: true, response }))
-				.catch((error) => ({ name: serviceName, success: false, error }))
-		}
-
-		const responses = await Promise.all([
-			promiseHandler(axiosASInstance.get('/health'), 'auth-service'),
-			promiseHandler(axiosFSInstance.get('/health'), 'file-service'),
-			promiseHandler(axiosMSInstance.get('/health'), 'main-service'),
-			promiseHandler(axiosMSGSInstance.get('/health'), 'message-service'),
-			promiseHandler(axiosNSInstance.get('/health'), 'notification-service'),
-		])
-
-		return responses
-	}
-
-	const {
-		data: responses,
-		isLoading,
-		error: reqError,
-		refetch,
-	} = useQuery(['fetch-health-services'], fetchServicesHealth, {
-		onError: () => { },
-		refetchInterval: 3.6e6,
-		refetchOnWindowFocus: false,
-	})
-
-	const services = responses?.map(({ name, success, response }, idx) => ({ name: name, isServiceON: success }))
-
-	const isAtLeastOneErrored = services?.some((s) => !s.isServiceON)
-
-	return { services, isLoading, reqError, isAtLeastOneErrored, refetch }
-}
-
-export default useFetchServicesHealth
->>>>>>> Stashed changes
