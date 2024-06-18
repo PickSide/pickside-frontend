82f9443e7f28bf9b8859fc2e2c0b92945d79585d
thenoobgrammer <thenoobgrammer@gmail.com>
2024-06-18 19:54:07 -0400

    fix(PICK-129):fix activity details

--- src/pages/Listing/components/Dialogs/ActivityDetailsDialog.tsx
+++ src/pages/Listing/components/Dialogs/ActivityDetailsDialog.tsx
@@ -6,48 +6,49 @@ import moment from 'moment'
 import { useTranslation } from 'react-i18next'
 
 const ActivityDetailsDialog: FC<{ activity: Activity }> = ({ activity }) => {
-    const { t } = useTranslation()
-    const date = new Date(activity.date)
-    const time = new Date(activity.time)
-    return (
-        <div className='flex flex-col'>
-            <div className='flex items-center justify-center mb-3'>
-                <Image />
-            </div>
-            <div className='grid grid-cols-3 grid-rows-1 gap-3 mb-3'>
-                <div className='row-span-1'>
-                    <p className='font-semibold'>{t('Address')}</p>
-                    <span>{activity.address}</span>
-                </div>
+	const { t } = useTranslation()
+	const date = new Date(activity.date)
+	const time = new Date(activity.time)
+	return (
+		<div className="block">
+			<div className="flex items-center justify-center mb-3">
+				<Image src={activity.images ? activity.images[0] : ''} />
+			</div>
+			<ul className="flex flex-col gap-y-4">
+				<li className="row-span-1">
+					<p className="font-semibold">{t('Address')}</p>
+					<span>{activity.address}</span>
+				</li>
 
-                <div className='row-span-1'>
-                    <p className='font-semibold'>{t('Price')}</p>
-                    <span>{activity.price <= 0 ? t('Free') : `$${activity.price} ${t('entry fee')}`}</span>
-                </div>
-                <div className='row-span-1'>
-                    <p className='font-semibold'>{t('Date')}</p>
-                    <p>{date.toDateString()}</p>
-                    <p>{moment(time).format('LT')}</p>
-                </div>
-            </div>
-            <div className='flex flex-col mb-3'>
-                <p className='font-semibold'>{t('Paticipants')}</p>
-                <div className='grid grid-flow-row grid-cols-2 gap-y-2'>
-                    {!!activity.participants?.length ? activity.participants?.map((p, idx) => (
-                        <div key={idx} className='inline-flex items-center gap-x-2'>
-                            <Avatar src={p.avatar} />
-                            <p>{p.fullName}</p>
-                        </div>
-                    )) : 'No participants'}
-                </div>
-
-            </div>
-            <div className='col-span-2 row-span-2'>
-                <p className='font-semibold'>{t('Rules')}</p>
-                <span>{activity.rules || t('No rules')}</span>
-            </div>
-        </div>
-    )
+				<li className="row-span-1">
+					<p className="font-semibold">{t('Price')}</p>
+					<span>{activity.price <= 0 ? t('Free') : `$${activity.price} ${t('entry fee')}`}</span>
+				</li>
+				<li className="row-span-1">
+					<p className="font-semibold">{t('Date')}</p>
+					<p>{date.toDateString()}</p>
+					<p>{moment(time).format('LT')}</p>
+				</li>
+				<li className="flex flex-col mb-3">
+					<p className="font-semibold">{t('Paticipants')}</p>
+					<div className="grid grid-flow-row grid-cols-2 gap-y-2">
+						{!!activity.participants?.length
+							? activity.participants?.map((p, idx) => (
+									<div key={idx} className="inline-flex items-center gap-x-2">
+										<Avatar src={p.avatar} />
+										<p>{p.fullName}</p>
+									</div>
+								))
+							: 'No participants'}
+					</div>
+				</li>
+				<li className="col-span-2 row-span-2">
+					<p className="font-semibold">{t('Rules')}</p>
+					<span>{activity.rules || t('No rules')}</span>
+				</li>
+			</ul>
+		</div>
+	)
 }
 
 export default ActivityDetailsDialog
